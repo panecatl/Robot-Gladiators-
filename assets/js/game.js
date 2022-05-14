ec266b796eb02a0949552ab3218bd2b91b44d884
panecatl <ypanecatl24@gmail.com>
2022-05-13 20:48:27 -0500

    random health and attack values.

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,7 @@ var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40, 60); 
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
@@ -24,14 +24,15 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 //subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
 
         //remove enemy's health by subtracting the amount set in the playerAttack variable 
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health reamining.'
         );
@@ -50,7 +51,8 @@ var fight = function(enemyName) {
         }
 
         //remove player's health by subtracting the amout set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage); 
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health reamining.'
         );
@@ -82,7 +84,7 @@ var startGame = function () {
             var pickedEnemyName = enemyNames[i];
 
             //reset enenmyhealth before starting a new fight
-            enemyHealth = 50;
+            enemyHealth = Math.floor(Math.random() * 21) + 40;
         
             //pass the pickedEnemyName variable into the fight funxtion
             fight(pickedEnemyName);
