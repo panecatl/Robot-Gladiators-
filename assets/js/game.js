5f54886a1b1f6063cd28e4ff55ba595ae8f219de
panecatl <ypanecatl24@gmail.com>
2022-05-15 20:51:04 -0500

    added integer values to shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -140,21 +140,22 @@ var endGame = function() {
 var shop = function() {
     //ask player what they would like to do 
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, 3 for LEAVE."
     );
+
+    //converts answer from prompt tp an actual number
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
     switch(shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-            case "UPGRADE":
-            case "upgrade":
+            case 2:
             playerInfo.upgradeAttack(); 
             break;
 
-            case "LEAVE":
-            case "leave":
+            case 3:
             window.alert("Leaving the store.");
             //do nothing, so function will end
             break;
